b351f489ef54e2644f06cb76f193a2a7e2bdb94a
Buganini Chiu <buganini@b612.tw>
2023-04-07 13:26:06 +0800

    fix crash for empty input

--- localization.py
+++ localization.py
@@ -74,6 +74,8 @@ def strip_note(s):
 def yes_or_no(question):
     while "the answer is invalid":
         reply = str(input(question+' (y/n): ')).lower().strip()
+        if not reply:
+            continue
         if reply[0] == 'y':
             return True
         if reply[0] == 'n':
